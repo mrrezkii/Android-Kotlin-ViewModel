b5773c71c901621060926217a25c68de6548448c
nando <muhammad.rezki.ananda@gmail.com>
2021-04-24 23:26:20 +0700

    Data Binding

--- app/src/main/java/com/newbiexpert/android_kotlin_learnviewmodel/MainActivity.kt
+++ app/src/main/java/com/newbiexpert/android_kotlin_learnviewmodel/MainActivity.kt
@@ -2,10 +2,20 @@ package com.newbiexpert.android_kotlin_learnviewmodel
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.newbiexpert.android_kotlin_learnviewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.countText.text = count.toString()
+        binding.button.setOnClickListener {
+            count++
+            binding.countText.text = count.toString()
+        }
     }
 }
