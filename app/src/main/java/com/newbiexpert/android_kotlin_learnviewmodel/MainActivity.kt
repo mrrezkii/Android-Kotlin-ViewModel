b556e2481d81625cdcf3497275da70ed33498a24
nando <muhammad.rezki.ananda@gmail.com>
2021-04-24 09:55:25 +0700

    INITIAL COMMIT

---
+++ app/src/main/java/com/newbiexpert/android_kotlin_learnviewmodel/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.newbiexpert.android_kotlin_learnviewmodel
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
