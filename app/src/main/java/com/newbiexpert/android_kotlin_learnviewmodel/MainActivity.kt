3200ec2ebd110d7603e8ba700063f8d824ebf4e6
nando <muhammad.rezki.ananda@gmail.com>
2021-04-24 23:35:31 +0700

    View Model

--- app/src/main/java/com/newbiexpert/android_kotlin_learnviewmodel/MainActivity.kt
+++ app/src/main/java/com/newbiexpert/android_kotlin_learnviewmodel/MainActivity.kt
@@ -3,19 +3,20 @@ package com.newbiexpert.android_kotlin_learnviewmodel
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.newbiexpert.android_kotlin_learnviewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var count = 0
+    private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.countText.text = count.toString()
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        binding.countText.text = viewModel.getCurrentCount().toString()
         binding.button.setOnClickListener {
-            count++
-            binding.countText.text = count.toString()
+            binding.countText.text = viewModel.getUpdateCount().toString()
         }
     }
 }
